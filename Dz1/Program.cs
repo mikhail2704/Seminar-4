f90acdc26602baa448468c99ce55c8a2297e08f5
Mikhail_Golovkov <mik270400@mail.ru>
2022-12-01 22:06:24 +0300

    Dz

--- Dz1/Program.cs
+++ Dz1/Program.cs
@@ -1,10 +1,13 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-Console.Write("Введите первое число:");
-double a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число:");
-double b = Convert.ToInt32(Console.ReadLine());
-double sim = Math.Pow(a,b);
-Console.Write($"{a}^{b}={sim}");
-f
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа:");
+int b = Convert.ToInt32(Console.ReadLine());
+int sum = 1;
+for (int i = 0; i<b ;i++)
+   {
+   sum=sum*a;
+   }
+Console.WriteLine(sum );
